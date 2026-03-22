d59afb28dfb5580294ba1eb3777e11bb4e4f129e
EmbustNews <imjustastick@gmail.com>
2026-03-22 15:09:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-document.querySelector(".share").addEventListener("click", () => {
-    navigator.share({
-        title: document.title,
-        url: window.location.href
-    });
-});
-</script>
